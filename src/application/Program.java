86c5f782bd824ba9d07d885812f8cedbdb4f9c81
BrunoBuilder <brunofernandotadeu@hotmail.com>
2021-05-25 21:25:17 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/yyyy HH:mm:ss");
+		
+		Date x1 = new Date();
+		
+		System.out.println("Hello, word!");
+		System.out.println(sdf.format(x1));
+
+	}
+
+}
